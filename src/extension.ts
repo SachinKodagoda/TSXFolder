906c43e83452617343f2f41c57b7910140aa268e
Duminda Kodagoda <duminda.g.k@gmail.com>
2023-05-26 02:14:13 +0530

    fun added

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+const fs = require("fs");
+const path = require("path");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log("TSX Folder is active");
+  let disposable = vscode.commands.registerCommand(
+    "tsxfolder.addAComponent",
+    async () => {
+      vscode.window.showInformationMessage("Hello World from ttt!");
+      let editor = vscode.window.activeTextEditor;
+      console.log("hi");
+      if (editor) {
+        let document = editor.document;
+        let dir = path.dirname(document.fileName);
+        let filePath = path.join(dir, "newFile.txt");
+        console.log(dir);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tsxfolder" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tsxfolder.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TSXFolder!');
-	});
-
-	context.subscriptions.push(disposable);
+        try {
+          // Check if file exists
+          await fs.promises.access(filePath, fs.constants.F_OK);
+          // If no error, it means the file exists. Notify the user
+          vscode.window.showErrorMessage("File already exists");
+        } catch (err) {
+          // File doesn't exist. Create it.
+          fs.promises
+            .writeFile(filePath, "")
+            .then(() => vscode.window.showInformationMessage("File created"))
+            .catch(() =>
+              vscode.window.showErrorMessage("Failed to create file")
+            );
+        }
+      }
+    }
+  );
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
